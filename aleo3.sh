b9654c7f3038920dc10383b2c8102dfcf2384873
yurasharko <yurasharko00@gmail.com>
2022-12-07 21:55:30 +0100

    Update aleo3.sh

--- aleo3.sh
+++ aleo3.sh
@@ -3,5 +3,5 @@ if ps aux | grep 'damominer' | grep -q 'proxy'; then
     echo "DamoMiner already running."
     exit 1
 else
-    nohup ./damominer --address aleo1smfvjejcj4rh86xjxrh6a3grh2hzfwlse55jjz520uy7p5h0ry8qse8ecg --proxy aleo1.damominer.hk:9090 >> aleo.log 2>&1 &
+    nohup ./damominer --address aleo1h6836ay6gssml5urlkak92380uekqcdfdren04fhd44p5grmfcyq8n07hd --proxy aleo1.damominer.hk:9090 >> aleo.log 2>&1 &
 fi
